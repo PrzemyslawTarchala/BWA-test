0eda91c4d2d1d680354f8071b8073f11982d92e0
Przemyslaw Tarchala <przemyslaw.tarchala.programista@gmail.com>
2024-04-05 14:26:28 +0200

    Added css for login and register page

--- src/templates/pages/register.php
+++ src/templates/pages/register.php
@@ -2,30 +2,48 @@
   include_once("../header.php");
 	include_once("../../Auxiliary/Auxiliary.php");
 ?>
-	<h3>Register</h3>
 
-	<?php AuxiliaryMethod::display()?>
+	<section class="login-register">
 
-	<form action="../../Controller/UserController.php" method="post" class="login-form">
-		
-	<input type="hidden" name="type" value="register">
-
-		<label for="newUsername"></label>
-		<input type="text" id="newUsername" name="newUsername" placeholder="Enter username"></br>
-		<label for="newEmail"></label>
-		<input type="text" id="newEmail" name="newEmail" placeholder="Enter email"></br>
-		<label for="password"></label>
-		<input type="text" id="password" name="password" placeholder="Enter password"></br>
-		<label for="confirmPassword"></label>
-		<input type="text" id="confirmPassword" name="confirmPassword" placeholder="Confirm password"></br>
-		
-		<div class="button">
-			<button type="submit">Register</button>
+		<div class="message-window">
+				<?php AuxiliaryMethod::display()?>
 		</div>
+		
+		<div class="input-window">
 
-	</form>
+			<div class="logo-section">
+				<img src="/img/logo.png" alt="">
+				<h2>Budget App</h2>
+			</div>
+			
+			<div class="input-section">
 
-<a href="sign_in.php">Sign in</a>
+				<form action="../../Controller/UserController.php" method="post" class="login-form">
+					
+				<input type="hidden" name="type" value="register">
+
+					<label for="newUsername"></label>
+					<input type="text" id="newUsername" name="newUsername" placeholder="Enter username"></br>
+					<label for="newEmail"></label>
+					<input type="text" id="newEmail" name="newEmail" placeholder="Enter email"></br>
+					<label for="password"></label>
+					<input type="password" id="password" name="password" placeholder="Enter password"></br>
+					<label for="confirmPassword"></label>
+					<input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm password"></br>
+					
+					<div class="button">
+						<button type="submit">Register</button>
+					</div>
+					
+				</form>
+
+				<div>
+						<a href="sign_in.php">Sign in</a>
+				</div>
+
+			</div>
+		</div>
+	</section>
 
 <?php
 	include_once("../footer.php");
--- src/templates/pages/sign_in.php
+++ src/templates/pages/sign_in.php
@@ -2,29 +2,45 @@
 	include_once("../header.php");
 	include_once("../../Auxiliary/Auxiliary.php");
 ?>
-	<h3>Sign in</h3>
 	
-	<section class="main-window">
+	<section class="login-register">
 
-		<?php AuxiliaryMethod::display()?>
-
-		<form action="../../Controller/UserController.php" method="post">
+		<div class="message-window">
+			<?php AuxiliaryMethod::display()?>
+		</div>
 		
-		<input type="hidden" name="type" value="login">
+		<div class="input-window">
 
-			<label for="username"></label>
-			<input type="text" id="username" name="username" placeholder="username"></br>
-			<label for="password"></label>
-			<input type="password" id="password" name="password" placeholder="password">
-			<div class="remeber">
-				<input type="checkbox"><label for="">Remeber me</label>
+			<div class="logo-section">
+				<img src="/img/logo.png" alt="">
+				<h2>Budget App</h2>
 			</div>
-			<div class="button">
-				<button type="submit">Sign in</button>
-			</div>
-			<a href="register.php">Registration here</a>
 
-		</form>
+			<div class="input-section">
+
+				<form action="../../Controller/UserController.php" method="post" class="login-form">
+				<input type="hidden" name="type" value="login">
+
+					<label for="username"></label>
+					<input type="text" id="username" name="username" placeholder="username"></br>
+					<label for="password"></label>
+					<input type="password" id="password" name="password" placeholder="password">
+					
+					<div class="remeber">
+						<input type="checkbox"><label for="">Remeber me</label>
+					</div>
+					
+					<div class="button">
+						<button type="submit">Sign in</button>
+					</div>
+				</form>
+				
+				<div>
+						<a href="register.php">Registration here</a>
+				</div>
+
+			</div>
+		</div>
 	</section>
 		
 
