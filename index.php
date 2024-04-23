585ac0f7d3091e2d42c54abe880e6481dd848cd4
Przemyslaw Tarchala <przemyslaw.tarchala.programista@gmail.com>
2024-04-23 19:26:42 +0200

    Added Excpetion classes

--- index.php
+++ index.php
@@ -4,20 +4,33 @@ declare(strict_types=1);
 
 session_start();
 
-
 require_once("src/Controller/AbstractController.php");
 require_once("src/Controller/AppController.php");
 require_once("src/Request.php");
-require_once("src/Utils/debug.php");
-
 require_once("src/View.php");
+require_once("src/Exception/AppException.php");
+require_once("src/Exception/ConfigurationException.php");
 
 use App\Controller\AbstractController;
 use App\Controller\AppController;
 use App\Request;
+use App\Exception\AppException;
+use App\Exception\ConfigurationException;
 
 $configuration = require_once("config/config.php");
 $request = new Request($_GET, $_POST, $_SERVER);
 
-AbstractController::initConfiguration($configuration);
-(new AppController($request)) -> run();
+try{
+	AbstractController::initConfiguration($configuration);
+	(new AppController($request)) -> run();
+} 
+
+catch(AppException $e){
+	echo "<h2>Application Error</h2><br>";
+	echo "$e";
+}
+
+catch(ConfigurationException $e){
+	echo "<h2>Application Error</h2><br>";
+	echo "$e";
+}
--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace App\Controller;
 
 require_once("src/Controller/UserController.php");
+require_once("src/Exception/ConfigurationException.php");
 
 use App\Request;
 use App\View;
 use App\Controller\UserController;
+use App\Exception\ConfigurationException;
 
 abstract class AbstractController
 {
@@ -21,6 +23,10 @@ abstract class AbstractController
 
 	public function __construct(Request $request)
 	{
+		if(empty(self::$configuration['db'])){
+			throw new ConfigurationException('Configuration Error');
+		}
+
 		$this->request = $request;
 		$this->view = new View();
 		$this->user = new UserController(self::$configuration['db']);
@@ -37,7 +43,7 @@ abstract class AbstractController
 		if(!method_exists ($this, $action)){
 			$action = self::DEFAULT_ACTION . 'Action';
 		}
-		$this->$action();
+		$this->$action();	
 	}
 
 	private function action(): string
